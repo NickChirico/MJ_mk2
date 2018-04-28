26d704e031a3669556bf53ca3716acfb04c53d10
MC <mcmailbox014@gmail.com>
2018-04-28 01:14:18 -0400

    Zombie Wall
    ZombieWall script has a gameobject that just moves to right at increasing speed, when it hits a player trigger it sets the player inactive, makes a different player (without controls and with a death animation)
    
    This separate player death animated object will reload the mainscene upon finishing the animation

---
+++ Assets/zombieWall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class zombieWall : MonoBehaviour {
+
+	public GameObject menuControl;
+	bool inMenu = true;
+
+	public GameObject mjDeath;
+
+	public float wallSpeed = 0;
+	public float speedI = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inMenu = menuControl.GetComponent<menuController> ().inMenu;
+
+		if (!inMenu) {
+			this.transform.position += Vector3.right * wallSpeed * Time.deltaTime;
+			wallSpeed += speedI;	//*****wallspeed increases over time
+		}
+	}
+
+	void OnTriggerEnter (Collider player)	{
+		mjDeath.SetActive (true);	//******mjDeath will always follow player, this means on death the player is destroyed and the animation object just appears; when it finishes the games reloads the scene
+		Destroy (player.gameObject);
+		//alternatively: player.SetActive (false);
+	}
+}
