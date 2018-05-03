6b45d0950217f52a793730145ef7dd15dd2b9ab8
Unknown <shl638@nyu.edu>
2018-05-03 00:54:53 -0400

    Parallax and map

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, transform.position.z);
+	}
+}
